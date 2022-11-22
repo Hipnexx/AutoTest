084be72907e416fee1db018e5fcea473587090b1
Hipnexx <115487665+Hipnexx@users.noreply.github.com>
2022-11-22 17:10:30 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -11,9 +11,12 @@ class TestCalc:
 
     def test_multiply_calculate_failed(self):
         assert self.calc.multiply(self, 2, 2) == 5
+        
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 6, 3) == 2
 
-    def test_subtraction_correctly(self):
+    def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 3, 1) == 2
 
-    def test_adding_correctly(self):
+    def test_adding_calculate_correctly(self):
         assert self.calc.adding(self, 3, 1) == 4
